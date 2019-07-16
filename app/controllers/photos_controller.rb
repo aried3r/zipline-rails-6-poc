8622d95ae4b6fe7247eddc6adfb3d8e0836de841
Anton Rieder <aried3r@gmail.com>
2019-07-16 21:27:21 +0200

    Use image URLs for testing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,24 @@
+require 'open-uri'
+
 class PhotosController < ApplicationController
   include ActionController::Streaming
   include Zipline
 
   def index
-    photos = Photo.all
-    files = photos.map do |photo|
+    image_urls = %w[
+      https://upload.wikimedia.org/wikipedia/commons/4/4c/%27Matijevic_Hill%27_Panorama_for_Rover%27s_Ninth_Anniversary_%28False_Color%29.jpg
+      https://upload.wikimedia.org/wikipedia/commons/f/f0/%27Matijevic_Hill%27_Panorama_for_Rover%27s_Ninth_Anniversary_%28Stereo%29.jpg
+      https://upload.wikimedia.org/wikipedia/commons/1/16/%27Matijevic_Hill%27_Panorama_for_Rover%27s_Ninth_Anniversary.jpg
+    ]
+
+    files = image_urls.map.with_index do |image_url, index|
       [
-        photo.image,
-        photo.image.original_filename,
-        modification_time: 1.day.ago
+        image_url,
+        "#{index}.jpeg",
+        modification_time: index.days.ago
       ]
     end
+
     zipline(files, 'photos.zip')
   end
 end
